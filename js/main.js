219994a1ae994b01226924b5666e8bb1e873472b
Celia <celia.conde@kairosds.com>
2018-04-22 20:22:42 +0200

    saving select value

--- js/main.js
+++ js/main.js
@@ -1,38 +1,26 @@
-'use strict';
-
+'use strict'
 
 //Recogemos el valor del input
 
 let usersList = [];
-let user = {
-    name: '',
-    group: ''
-}
+
 
 function addUser () {
-    const userName = document.querySelector('.user__name').value;
+    let user = {
+        name: '',
+        group: ''
+    };
+
+    let userName = document.querySelector('.user__name').value;
+    let group = document.querySelector('.select_groups').value;
+
     user.name = userName;
+    user.group = group;
     usersList.push(user);
+
+    console.log(usersList);
+    
 }
 
 const addUserBtn = document.querySelector('.addUser__btn');
 addUserBtn.addEventListener('click', addUser);
-
-
-
-
-
-
-
-
-
-// <div class="description"><p>Herramienta para la gestión de grupos y usuarios</p></div>
-//         <input type="text" id="user__name" class="user__name" placeholder="Esribe tu nombre de usuario">
-//         <select class="select_groups" id="select_groups">
-//                 <option value="Grupo1">Grupo 1</option>
-//                 <option value="Grupo2">Grupo 2</option>
-//                 <option value="Grupo3">Grupo 3</option>
-//         </select>
-//             <div class="box1"></div>
-//             <div class="box2"></div>
-//             <div class="box3"></div>
